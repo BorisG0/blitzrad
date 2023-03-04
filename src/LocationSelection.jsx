9fe57fefbef7395bade7de8c2d3c4bca7e8317d3
Boris Gratchev <boris@gratchev.com>
2023-03-04 14:44:16 +0100

    location selection possible

--- src/LocationSelection.jsx
+++ src/LocationSelection.jsx
@@ -1,9 +1,19 @@
-import {List, ListItem, ListItemText } from '@mui/material';
+import * as React from 'react';
+import {List, ListItem, ListItemButton, ListItemText } from '@mui/material';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { getDistance } from 'geolib';
 import firestore from "./firebase"
 
+
+
 export function LocationSelection(){
+    const [selectedLocation, setSelectedLocation] = React.useState(1);
+
+    const handleLocationClick = (event, name) => {
+        console.log("handleLocationClick")
+        setSelectedLocation(name);
+    }
+
     const locationsRef = firestore.collection('locations');
     const query = locationsRef.limit(25);
     const [locations] = useCollectionData(query, {idField: 'id'});
@@ -12,7 +22,7 @@ export function LocationSelection(){
         <div>
             <h2>Location Selection</h2>
             <List>
-                {locations && locations.map(l => <LocationDisplay key={l.name} loc={l} />)}
+                {locations && locations.map(l => <LocationDisplay key={l.name} loc={l} clickEvent={handleLocationClick} sLoc={selectedLocation}/>)}
             </List>
         </div>
     )
@@ -28,9 +38,15 @@ export function LocationSelection(){
     console.log("dist: ", dist)
   
     return (
-        <ListItem button>
+        <ListItemButton onClick={(event) => props.clickEvent(event, name)}>
             <ListItemText  secondaryTypographyProps={{ sx: { color: "white" } }}
-             primary={name} secondary={"📍 "+dist+"m"}/>
-        </ListItem>
+             primary={name}
+             secondary={
+                <div>
+                    <>📍 {dist}m</>
+                    <p>{(props.sLoc == name) ? "lol": null}</p>
+                </div>
+             }/>
+        </ListItemButton>
     )
   }
