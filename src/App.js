a3f91f4982098c8bbb20cb35d44efa327de90b13
LucaGrab <luca@grabowski.com>
2023-03-07 22:26:32 +0100

    added paypal payment on route for testing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 import { NavBar } from './NavBar';
 import { AboutUs } from './AboutUs';
 import { Account } from './Account';
+import { Payment } from './Payment';
 
 
 function App() {
@@ -19,6 +20,8 @@ function App() {
           <Route path="/region" element={<RegionSelection />} />
           <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/account" element={<Account />} />
+          <Route path="/payment" element={<Payment />} />
+
 
        </Routes>
       </div>
---
+++ src/Payment.jsx
@@ -0,0 +1,30 @@
+import {PayPalScriptProvider, PayPalButtons} from '@paypal/react-paypal-js';
+
+export function Payment() {
+
+    const paypalOptions = {
+        "client-id": "AdCgq612duLsoz1YMIH8B4tSNBpWtJtlfXE0TZ1wGIegRuDKc7Sg1Y95gq7Z_24rlieuHC6pKsbrWgzi", 
+        "currency": "EUR"
+    }
+
+    return (
+        <PayPalScriptProvider options={paypalOptions}>
+            <PayPalButtons           createOrder={(data, actions) => {
+            return actions.order.create({
+              purchase_units: [
+                {            
+                  amount: {
+                    value: "13.99",
+                  },
+                },
+              ],
+            });
+          }}
+          onApprove={async (data, actions) => {
+            const details = await actions.order.capture();
+            const name = details.payer.name.given_name;
+            alert("Transaction completed by " + name);
+          }}/>
+        </PayPalScriptProvider>
+    )
+}
