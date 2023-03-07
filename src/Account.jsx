d2b73a31a2ee9e668bc8be1132cacf88fb459c12
LucaGrab <luca@grabowski.com>
2023-03-07 10:15:30 +0100

    daten in bestell übersicht aber nicht schön

--- src/Account.jsx
+++ src/Account.jsx
@@ -9,82 +9,80 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import React, { useState, useEffect } from 'react';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import firebase from './firebase';
+import './Account.css';
+import { Divider } from '@mui/material';
 
 const firestore = firebase.firestore();
 
 const auth = firebase.auth();
 
 export function Account() {
-    //user geht auch nicht wenn man seite neu lädt
     const [user] = useAuthState(auth)
 
     const bookingsRef = firestore.collection('bookings');
 
-    const query = bookingsRef.where("uId", "==", "crW5255058ReQgNMrQ9R3eks7Op1");
+    const query = bookingsRef.where("uId", "==", "crW5255058ReQgNMrQ9R3eks7Op1");//.orderBy("bookedAt", "desc")
+
 
     const [bookings] = useCollectionData(query, { idField: 'id' });
-    console.log(bookings)
-    //const [bookingData,setBookingData]= useState([]);
-
-    /*firebase.firestore().collection("bookings").where("uId", "==", "crW5255058ReQgNMrQ9R3eks7Op1").get()
-    .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            //setBookingData([doc.data().Adress]);
-            var tempBookingData = doc.data();
-            setBookingData(arr => [...arr , tempBookingData]);
-            console.log(tempBookingData)
-        });
-    })
-    .catch((error) => {
-        console.log("Error getting documents: ", error);
-    });*/
-
-
 
     return (
-        <Grid2 container spacing={2}>
-            <Grid2 xs={12} md={6}>
+<>
                 <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
                     Your Bookings
                 </Typography>
-                <List >
+                <List id="test" sx={{position:"center", margin: "0 auto"}}  >
                     {bookings && bookings.map(booking =>
-                        <ListItem key={booking.bookedAt}>
-                            <ListItemText
-                            //parse date to string
-
-
-                                primary={booking.bookedAt.toString()}
-                                secondary="Date: 12.01.2023"
-                            />
+                        <ListItem key={booking.bookedAt} divider={true} dense={true} sx={{border:0.5, textAlign: "center"}}>
+                            <Grid2  container rowSpacing={1} columnSpacing={20} columns={2}>
+                                <Grid2 xs={1} sx={{textAlign:"right"}}>
+                                    Location
+                                </Grid2>
+                                <Grid2 xs={1} sx={{textAlign:"left"}}>
+                                    {booking.location}
+                                </Grid2>
+                                <Grid2 xs={1}>
+                                    Booked at
+                                </Grid2>
+                                <Grid2 xs={1}>
+                                    {booking.bookedAt.toDate().getDate() + ". "
+                                        + booking.bookedAt.toDate().getMonth() + '. '
+                                        + booking.bookedAt.toDate().getFullYear() + " "
+                                        + booking.bookedAt.toDate().getHours() + ":"
+                                        + booking.bookedAt.toDate().getMinutes()}
+                                </Grid2>
+                                <Grid2 xs={1}>
+                                    Rent start
+                                </Grid2>
+                                <Grid2 xs={1}>
+                                    {booking.rentStart.toDate().getDate() + ". "
+                                        + booking.rentStart.toDate().getMonth() + '. '
+                                        + booking.rentStart.toDate().getFullYear() + " "
+                                        + booking.rentStart.toDate().getHours() + ":"
+                                        + booking.rentStart.toDate().getMinutes()}
+                                </Grid2>
+                                <Grid2 xs={1}>
+                                    Rent end
+                                </Grid2>
+                                <Grid2 xs={1}>
+                                    {booking.rentEnd.toDate().getDate() + ". "
+                                        + booking.rentEnd.toDate().getMonth() + '. '
+                                        + booking.rentEnd.toDate().getFullYear() + " "
+                                        + booking.rentEnd.toDate().getHours() + ":"
+                                        + booking.rentEnd.toDate().getMinutes()}
+                                </Grid2>
+                                <Grid2 xs={1}>
+                                    Type
+                                </Grid2>
+                                <Grid2 xs={1}>
+                                    {booking.type}
+                                </Grid2>
+                            </Grid2>
+                            <Divider/>
                         </ListItem>
                     )}
 
                 </List>
-            </Grid2>
-            <Grid2 xs={12} md={6}>
-                <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-                    Personal Information
-                </Typography>
-                <List >
-                    <ListItem >
-                    {user? user.displayName : "your name" }
-                        <TextField
-                            disabled
-                            id="filled-disabled"
-                            label="Name"
-                            defaultValue={user? user.displayName : "your name" }
-                            variant="filled"
-                        >
-                                                        {user? user.displayName : "t" }
-
-                        </TextField>
-                    </ListItem>
-
-
-                </List>
-            </Grid2>
-        </Grid2>
+                </>
     )
 }
