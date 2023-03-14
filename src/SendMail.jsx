2f014300adeddc8aa2039cb42b167c34786b8c46
LucaGrab <luca@grabowski.com>
2023-03-14 20:07:42 +0100

    send mail without form

--- src/SendMail.jsx
+++ src/SendMail.jsx
@@ -4,35 +4,22 @@ import firebase from './firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 export const SendMail = () => {
-  const form = useRef();
-
-const sendThisMail = () => {
-
-}
-const auth = firebase.auth();
-
-const [user] = useAuthState(auth);
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm('service_ebqx6vr', 'template_blgchp8', form.current, 'p9vHFTrfHp5W4igrO')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-  };
-
-  return (
-    <>
-    <button onClick={sendThisMail}> tst send mail</button>
-    <form ref={form} onSubmit={sendEmail} type="hidden">
-      <input type="hidden" defaultValue={user.displayName} name="user_name" />
-      <input type="hidden" defaultValue={user.email} name="user_email" />
-      <input type="hidden" defaultValue={"15€"} name="price" />
-      <input type="submit" value="Send" />
-    </form>
-    </>
-  );
+    const auth = firebase.auth();
+    const [user] = useAuthState(auth);
+    const sendThisMail = () => {
+        var templateParams = {
+            user_name: user.displayName,
+            user_email: user.email,
+            price: '15€'
+        };
+        emailjs.send('service_ebqx6vr', 'template_blgchp8', templateParams, 'p9vHFTrfHp5W4igrO')
+            .then(function (response) {
+                console.log('SUCCESS!', response.status, response.text);
+            }, function (error) {
+                console.log('FAILED...', error);
+            });
+    }
+    return (
+        <button onClick={sendThisMail}> tst send mail</button>
+    );
 };
