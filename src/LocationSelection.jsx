285cb38a52115f4710e360e01ac798d166fe1c49
Boris Gratchev <boris@gratchev.com>
2023-03-15 14:51:30 +0100

    saving bookings in db by location

--- src/LocationSelection.jsx
+++ src/LocationSelection.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import {List, ListItem, ListItemButton, ListItemText, Button } from '@mui/material';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { getDistance } from 'geolib';
 import firebase from "./firebase"
@@ -9,14 +9,31 @@ const firestore = firebase.firestore();
 
 export function LocationSelection(props){
     const locationsRef = firestore.collection('locations');
+    const bookingsRef = firestore.collection('bookings');
+
     const query = locationsRef.limit(25);
     const [locations] = useCollectionData(query, {idField: 'id'});
+
+    const saveBooking = async (e) => {
+        e.preventDefault();
+    
+        //const { uid } = auth.currentUser;
+
+        await bookingsRef.add({
+          bookedAt: firebase.firestore.FieldValue.serverTimestamp(),
+          uId: "test user",
+          type: "Bike",
+          location: props.selectedLocation
+        })
+      }
 
     return(
         <div>
             <h2>Location Selection</h2>
             <List>
-                {locations && locations.map(l => <LocationDisplay key={l.name} loc={l} clickEvent={props.handleLocationClick} sLoc={props.selectedLocation}/>)}
+                {locations && locations.map(l => <LocationDisplay key={l.name} loc={l}
+                clickEvent={props.handleLocationClick} sLoc={props.selectedLocation}
+                saveBooking={saveBooking}/>)}
             </List>
         </div>
     )
@@ -37,7 +54,10 @@ export function LocationSelection(props){
                 <>
                     <>📍 {dist}m</>
                     <br/>
-                    <>{(props.sLoc == name) ? "lol": null}</>
+                    <>{(props.sLoc == name) ? (<>
+                    lol
+                    <Button variant="contained" onClick={props.saveBooking}>book</Button>
+                    </>): null}</>
                 </>
              }/>
         </ListItemButton>
