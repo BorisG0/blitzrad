9f911bf0c0c3cd5e7bf7abf783c3406c26929e43
LucaGrab <luca@grabowski.com>
2023-03-17 17:21:47 +0100

    autocomplete in region selection

--- src/RegionSelection.jsx
+++ src/RegionSelection.jsx
@@ -1,5 +1,17 @@
-export function RegionSelection(){
-    return(
-        <h1>select region</h1>
+import { Autocomplete } from '@mui/material';
+import { TextField } from '@mui/material';
+
+export function RegionSelection() {
+    return (
+        <>
+            <h1>select region</h1>
+            <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={['Mannheim', 'Berlin', 'München']}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="Region" />}
+            />
+        </>
     )
 }
