e35717445a1504b16b7d5b32f42bb812b42796de
LucaGrab <luca@grabowski.com>
2023-03-17 11:11:30 +0100

    account seite aufgeräumt und div wenn kein user

--- src/Account.jsx
+++ src/Account.jsx
@@ -36,7 +36,6 @@ function qrCode(url) {
             level={"H"}
         />)
 }
-const number = 123
 const auth = firebase.auth();
 function getNiceDate(date) {
     let dd = date.toDate().getDate()
@@ -47,48 +46,15 @@ function getNiceDate(date) {
     return dd + "." + mm + "." + yyyy + " " + hh + ":" + minmin
 }
 export function Account() {
-    /*const [tests, setTests] = useState([]);
-    useEffect(() => {
-        let user = firebase.auth().currentUser;
-        const database = firebase.firestore();
-        const unsubscribe = database
-          .collection("bookings")
-          .where("uId", "==", user.uid)
-          .onSnapshot((snapshot) => {
-            setTests(snapshot.docs.map((doc) => doc.data()));
-            console.log(tests)
-
-          });
-    
-        return () => {
-          unsubscribe();
-        };
-      }, []);*/
-
-
-
-
     const [query, setQuery] = useState()
     const [bookings] = useCollection(query, { idField: 'id' });
     const [user, loading, error] = useAuthState(auth);
-    let dataArray = []
     if (loading) {
         return <div> Loading... </div>;
     } else if (user) {
         const bookingsRef = firestore.collection('bookings');
         if (query == undefined) {
             setQuery(bookingsRef.where("uId", "==", user.uid));//.orderBy("bookedAt", "desc")
-            /*firebase.firestore().collection("bookings").where("uId", "==", user.uid).get()
-            .then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    dataArray.push({id: doc.id, data: doc.data()})
-                    console.log(dataArray)
-                    // doc.data() is never undefined for query doc snapshots
-                });
-            })
-            .catch((error) => {
-                console.log("Error getting documents: ", error);
-            });*/
         }
         return (
             <>
@@ -112,9 +78,7 @@ export function Account() {
                                     </Grid2>
                                     <Grid2 xs={6}>
                                         {getNiceDate(booking.data().bookedAt)}
-
                                     </Grid2>
-
                                     <Grid2 xs={6}>
                                         Rent start
                                     </Grid2>
@@ -141,7 +105,6 @@ export function Account() {
                             <Button component={Link} to={`/scanned/${booking.id}`}> mein Button </Button>
                         </ListItem>
                     )}
-
                 </List>
             </>
         )
@@ -149,79 +112,6 @@ export function Account() {
         console.log("error")
         return <div>There was an authentication error.</div>;
     } else {
-        console.log("was hier los")
-        return <div> loading page </div>;
+        return <div> Nothing here... maybe you are not logged in? </div>;
     }
 }
-
-/*export function Account() {
-
-    const [user] = useAuthState(auth)
-
-    const bookingsRef = firestore.collection('bookings');
-
-    const query = bookingsRef.where("uId", "==", "crW5255058ReQgNMrQ9R3eks7Op1");//.orderBy("bookedAt", "desc")
-
-
-    const [bookings] = useCollectionData(query, { idField: 'id' });
-
-    return (
-<>
-                <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-                    Your Bookings
-                </Typography>
-                <List id="test" sx={{position:"center", margin: "0 auto"}}  >
-                    {bookings && bookings.map(booking =>
-                        <ListItem key={booking.bookedAt} divider={true} dense={true} sx={{border:0.5, textAlign: "center"}}>
-                            <Grid2  container rowSpacing={1} columnSpacing={20} columns={2}>
-                                <Grid2 xs={1} sx={{textAlign:"right"}}>
-                                    Location
-                                </Grid2>
-                                <Grid2 xs={1} sx={{textAlign:"left"}}>
-                                    {booking.location}
-                                </Grid2>
-                                <Grid2 xs={1}>
-                                    Booked at
-                                </Grid2>
-                                <Grid2 xs={1}>
-                                    {booking.bookedAt.toDate().getDate() + ". "
-                                        + booking.bookedAt.toDate().getMonth() + '. '
-                                        + booking.bookedAt.toDate().getFullYear() + " "
-                                        + booking.bookedAt.toDate().getHours() + ":"
-                                        + booking.bookedAt.toDate().getMinutes()}
-                                </Grid2>
-                                <Grid2 xs={1}>
-                                    Rent start
-                                </Grid2>
-                                <Grid2 xs={1}>
-                                    {booking.rentStart.toDate().getDate() + ". "
-                                        + booking.rentStart.toDate().getMonth() + '. '
-                                        + booking.rentStart.toDate().getFullYear() + " "
-                                        + booking.rentStart.toDate().getHours() + ":"
-                                        + booking.rentStart.toDate().getMinutes()}
-                                </Grid2>
-                                <Grid2 xs={1}>
-                                    Rent end
-                                </Grid2>
-                                <Grid2 xs={1}>
-                                    {booking.rentEnd.toDate().getDate() + ". "
-                                        + booking.rentEnd.toDate().getMonth() + '. '
-                                        + booking.rentEnd.toDate().getFullYear() + " "
-                                        + booking.rentEnd.toDate().getHours() + ":"
-                                        + booking.rentEnd.toDate().getMinutes()}
-                                </Grid2>
-                                <Grid2 xs={1}>
-                                    Type
-                                </Grid2>
-                                <Grid2 xs={1}>
-                                    {booking.type}
-                                </Grid2>
-                            </Grid2>
-                            <Divider/>
-                        </ListItem>
-                    )}
-
-                </List>
-                </>
-    )
-}*/
