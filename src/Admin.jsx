6434b606fa090475956e2ade6c144837fc45f3d1
LucaGrab <luca@grabowski.com>
2023-03-21 13:26:58 +0100

    price in admin page

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,5 +1,36 @@
+import firebase from "./firebase";
+import { useCollectionData, useCollection } from "react-firebase-hooks/firestore";
+import { doc, updateDoc } from "firebase/firestore";
+
+
 export function Admin() {
+    const firestore = firebase.firestore();
+
+    const auth = firebase.auth();
+
+    const vehilcesRef = firestore.collection('vehicles');
+    const query = vehilcesRef.limit(25);
+    const [vehicles] = useCollection(query, {idField: 'id'});
+
+    const testRef = doc(firestore, "vehicles", "scooter");
+
+    //const { uid } = auth.currentUser;
+    const changePrice = async (e) => {
+        /*e.preventDefault();
+
+        await vehilcesRef.add({
+          type: "testtype",
+          price: 10,
+        })*/
+        await updateDoc(testRef, {
+            price: 17
+          });
+      }
     return(
-        <div> admin </div>
+        <div>
+            admin
+            {vehicles && vehicles.docs.map(v => <div key={v.id}>{v.id} {v.data().price} </div>)}
+            <button onClick={changePrice}> change price</button>
+        </div>
     )
 }
